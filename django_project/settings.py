6a9db878381b5651a69b1cb9d76cfb8a98d3c160
root <knand4930@gmail.com>
2023-06-20 07:13:46 +0000

    update frontmaster and settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -164,15 +164,15 @@ CKEDITOR_UPLOAD_PATH = "uploads/"
 
 AUTH_USER_MODEL = 'account.User'
 
-# CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_CREDENTIALS = True
 #
-# CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_ALLOW_ALL = True
 #
 # # CORS_ALLOW_CREDENTIALS = True
 #
-# CSRF_TRUSTED_ORIGINS = ['https://qcenthotels.com']
-#
-# CSRF_COOKIE_DOMAIN = 'qcenthotels.com'
+CSRF_TRUSTED_ORIGINS = ['https://qcenthotels.com']
+
+CSRF_COOKIE_DOMAIN = 'qcenthotels.com'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
